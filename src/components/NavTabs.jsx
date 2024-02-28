56b67ea3f5be3b3e6a074df93ab5dbcc0b2ac0b2
Aliriza Bagdat <ambagdat@gmail.com>
2024-02-28 05:06:26 +0000

    Header included

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,8 +1,12 @@
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
+import Header from "./Header";
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Header />
+      </li>
       <li className="nav-item">
         <a
           href="#home"
