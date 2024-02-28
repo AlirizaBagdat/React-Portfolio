32da2a6c575c118b73359b713c1631ac8fc3df44
Aliriza Bagdat <ambagdat@gmail.com>
2024-02-28 05:21:09 +0000

    imported Footer

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import Footer from './Footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -30,6 +31,8 @@ export default function PortfolioContainer() {
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
       <main className="mx-3">{renderPage()}</main>
+      <Footer />
     </div>
+
   );
 }
