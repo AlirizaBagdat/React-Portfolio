d8b65f30d7285ef25e644bf4f7cdc074d7427d64
Aliriza Bagdat <ambagdat@gmail.com>
2024-02-28 05:17:17 +0000

    Footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export default function Footer(){
+    return (
+        <footer class="footer">
+        <ul>
+            <li>
+                <a href="https://github.com/AlirizaBagdat?tab=repositories">
+                <img class="github" src="./assets/github.png" alt="Github logo"/></a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/aliriza-bagdat-b3a109291/"></a>
+                <img src="./assets/linkedin.png" alt="LinkedIn Logo" />
+            </li>
+        </ul>  
+        </footer>
+    );
+}
