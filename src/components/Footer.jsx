c878d7b69402960c586408eb198bafd3f68de232
Aliriza Bagdat <ambagdat@gmail.com>
2024-03-01 00:17:32 +0000

    Footer aligned

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,11 @@
-import React from "react";
+import '../styles/Footer.css';
 export default function Footer(){
     return (
-        <footer class="footer">
-        <ul>
-            <li>
+        <footer class="footer">                
                 <a href="https://github.com/AlirizaBagdat?tab=repositories">
-                <img class="github" src="src\assets\github.png" alt="Github logo"/></a>
-            </li>
-            <li>
-                <a href="https://www.linkedin.com/in/aliriza-bagdat-b3a109291/"></a>
-                <img src="src/assets/linkedin.png" alt="LinkedIn Logo" />
-            </li>
-        </ul>  
+                <img class="github" src="src\assets\github.png" alt="Github logo"/></a>            
+                <a href="www.linkedin.com/in/aliriza-bagdat-b3a109291"></a>
+                <img src="src/assets/linkedin.png" alt="LinkedIn Logo" />        
         </footer>
     );
 }
