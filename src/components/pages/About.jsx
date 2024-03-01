b4c1bf876c0a5f87584514d5f3fe31f81d3885f2
Aliriza Bagdat <ambagdat@gmail.com>
2024-03-01 00:17:54 +0000

    About text replaced

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,19 +1,24 @@
+import '../../styles/about.css'
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+    <div className='aboutdiv'>
+      <h1 className="about">About </h1>
+      <p className="aboutp">
+      As a full stack developer, I'm on a mission to craft digital experiences that transcend the ordinary. With
+       a diverse skill set that spans both front-end and back-end development, I relish the art of bringing ideas to life in the digital realm.
+
+      My journey in the world of web development has been marked by a relentless pursuit of excellence. I thrive on overcoming challenges,
+       whether it's designing elegant user interfaces, optimizing server performance, or creating seamless database interactions.
+
+      My passion for code is matched only by my commitment to user-centric design. I believe that technology should serve as a bridge, 
+      making complex processes simple and enjoyable for the end user.
+
+      Throughout my career, I've had the privilege of working on a variety of projects, each presenting its unique set of opportunities 
+      and complexities. This journey has enriched me with invaluable experience and a deep understanding of the ever-evolving tech landscape.
+
+      Beyond coding, I'm an avid learner and a firm believer in staying on the cutting edge of industry trends. My dedication to continuous 
+      growth is fueled by the excitement of staying ahead of the curve and finding innovative solutions to modern challenges.
+
       </p>
     </div>
   );
